3749242a488e494e3fe198700f46c0180a3143ac
Marcus Barnes <mavjbarnes@gmail.com>
2015-12-13 21:40:46 -0500

    added autoprefixer to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const jade = require('gulp-jade');
+const autoprefixer = require('gulp-autoprefixer');
 
 /**
  * Static Server + watching scss/html files
@@ -32,6 +33,7 @@ gulp.task('jade', function() {
 gulp.task('sass', () => {
     return gulp.src('assets/css/main.scss')
       .pipe(sass())
+      .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
       .pipe(gulp.dest('assets/css'))
       .pipe(browserSync.stream())
 });
